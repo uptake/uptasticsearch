9fe9a22e0ba565dc9a6969a489429a59901dc376
James Lamb <jaylamb20@gmail.com>
2018-05-28 18:17:59 -0500

    Added a _refresh call to testing scripts to avoid false positives from slow starts

--- setup_es.sh
+++ setup_es.sh
@@ -24,16 +24,17 @@ curl --silent \
 # Upload data
 curl --silent \
      -X POST "http://${ES_HOST}/shakespeare/_bulk" \
-     --data-binary @sample.json \
-     -H 'Content-Type: application/json'
+     -H 'Content-Type: application/json' \
+     --data-binary @sample.json
      
-
-
 # Add an intentionally empty index
 curl --silent \
      -X PUT "http://${ES_HOST}/empty_index" \
      -H 'Content-Type: application/json' \
      -d @shakespeare_mapping.json
+
+# Refresh all indices
+curl -X POST "http://${ES_HOST}:9200/_refresh"
 
 # Check that we got something
 curl -X GET "http://${ES_HOST}/shakespeare/_search?size=1"
