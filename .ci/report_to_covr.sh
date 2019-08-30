389cc59c89c87a3b3831ad98e12e5e1668cdaeaf
James Lamb <jaylamb20@gmail.com>
2019-08-29 23:43:40 -0500

    simplified CI setup

---
+++ .ci/report_to_covr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# failure is a natural part of life
+set -e
+
+if [[ "$TASK" == "rpkg" ]]; then
+  Rscript -e " \
+    install.packages('covr'); \
+    Sys.setenv(NOT_CRAN = 'true'); \
+    covr::codecov('r-pkg/') \
+    "
+fi
+
+if [[ "$TASK" == "pypkg" ]]; then
+  echo "This is a Python build. No post-build actions configured."
+fi
