26d0032ad9b2a9156acdec19b005aa9efead69f1
James Lamb <jaylamb20@gmail.com>
2020-09-06 23:55:50 -0500

    [ci] upgrade CI to Ubuntu 18.04

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -2,11 +2,6 @@
 # failure is a natural part of life
 set -e
 
-# Every build needs Java to build and run Elasticsearch.
-# As of this writing, the R + Ubuntu images only have Java8
-# available to them
-export JAVA_APT_PKG="oracle-java8-set-default"
-
 # If language: r,
 # install these testing packages we need
 if [[ "$TASK" == "rpkg" ]];
@@ -15,18 +10,11 @@ then
   cp test-data/* r-pkg/inst/testdata/
 fi
 
-# If language: python,
-# install these testing packages we need
-if [[ "$TASK" == "pypkg" ]];
-then
-  export JAVA_APT_PKG="oracle-java9-set-default"
-fi
-
-# Install java
+# Every build needs Java to build and run Elasticsearch.
 sudo -E \
   apt-get \
       -yq \
       --no-install-suggests \
       --no-install-recommends \
       install \
-          ${JAVA_APT_PKG}
+          default-jdk
