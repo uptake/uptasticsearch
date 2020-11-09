32679dbdf6fb232c2634d8b7dd1ef6a5697ab4e8
James Lamb <jaylamb20@gmail.com>
2020-11-08 22:30:46 -0600

    more stuff

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -6,12 +6,6 @@ set -e
 if [[ "$TASK" == "rpkg" ]];
 then
 
-    export R_LIBS=${HOME}/Rlib
-    export R_LINUX_VERSION="4.0.3-1.1804.0"
-    export R_APT_REPO="bionic-cran40/"
-
-    mkdir -p ${R_LIBS}
-
     # `devscripts` is required for 'checkbashisms' (https://github.com/r-lib/actions/issues/111)
     if [[ $OS_NAME == "linux" ]]; then
         sudo apt-get update
