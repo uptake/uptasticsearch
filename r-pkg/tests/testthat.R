0564daea32a86281b771b435265a65f5dea8bdc7
James Lamb <jaylamb20@gmail.com>
2020-11-09 00:46:59 -0600

    revert unnecessary change

--- r-pkg/tests/testthat.R
+++ r-pkg/tests/testthat.R
@@ -1,3 +1,6 @@
+# Note that you would never run this file directly. This is used by tools::testInstallPackages()
+# and other packages like covr.
+
 # This line ensures that R CMD check can run tests.
 # See https://github.com/hadley/testthat/issues/144
 Sys.setenv("R_TESTS" = "")
