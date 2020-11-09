5912dad89b8e08771d73d428f003b7bb33942241
James Lamb <jaylamb20@gmail.com>
2020-11-08 22:40:11 -0600

    pdflatex

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -7,17 +7,20 @@ if [[ "$TASK" == "rpkg" ]];
 then
 
     # `devscripts` is required for 'checkbashisms' (https://github.com/r-lib/actions/issues/111)
-    if [[ $OS_NAME == "linux" ]]; then
-        sudo apt-get update
-        sudo apt-get install \
-            --no-install-recommends \
-            -y \
-            --allow-downgrades \
-                libcurl4-openssl-dev \
-                curl \
-                devscripts \
-                || exit -1
-    fi
+    sudo apt-get update
+    sudo apt-get install \
+        --no-install-recommends \
+        -y \
+        --allow-downgrades \
+            libcurl4-openssl-dev \
+            curl \
+            devscripts \
+            texinfo \
+            texlive-latex-recommended \
+            texlive-fonts-recommended \
+            texlive-fonts-extra \
+            qpdf \
+        || exit -1
 
     Rscript -e "install.packages(c('assertthat', 'covr', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'lintr', 'purrr', 'rmarkdown', 'stringr', 'testthat', 'uuid'), repos = 'https://cran.r-project.org')"
     cp test-data/* r-pkg/inst/testdata/
