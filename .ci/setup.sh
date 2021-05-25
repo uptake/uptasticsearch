7ef80b680894481fb124d20103118c740861f38a
James Lamb <jaylamb20@gmail.com>
2021-05-24 21:57:05 -0500

    just use the jim hester one

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -22,8 +22,6 @@ then
             qpdf \
         || exit -1
 
-    conda install -y -c conda-forge 'pandoc>1.12.3'
-
     Rscript -e "install.packages(c('assertthat', 'covr', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'lintr', 'purrr', 'rmarkdown', 'stringr', 'testthat', 'uuid'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())"
     cp test-data/* r-pkg/inst/testdata/
 fi
