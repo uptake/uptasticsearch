07bbd9ed332f05fe88377f6fdfd45176fa9c2fbd
James Lamb <jaylamb20@gmail.com>
2025-01-31 00:10:29 -0600

    more packages

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -17,7 +17,7 @@ sudo apt-get install \
     texlive-fonts-extra \
     tidy
 
-Rscript -e "install.packages(c('assertthat', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'markdown', 'purrr', 'stringr', 'uuid'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())"
+Rscript -e "install.packages(c('assertthat', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'markdown', 'pkgdown', 'purrr', 'roxygen2', 'stringr', 'uuid'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())"
 
 pushd ./r-pkg
 R CMD INSTALL --with-keep.source .
