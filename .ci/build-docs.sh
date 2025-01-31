fffb17c989a6d412d93acdcba33f6f0e779ee9f7
James Lamb <jaylamb20@gmail.com>
2025-01-30 23:38:53 -0600

    use a dedicated script

---
+++ .ci/build-docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# failure is a natural part of life
+set -e -u -o pipefail
+
+# `devscripts` is required for 'checkbashisms' (https://github.com/r-lib/actions/issues/111)
+sudo apt-get update
+sudo apt-get install \
+    --no-install-recommends \
+    -y \
+    --allow-downgrades \
+    libcurl4-openssl-dev \
+    curl \
+    texinfo \
+    texlive-latex-recommended \
+    texlive-fonts-recommended \
+    texlive-fonts-extra \
+    tidy
+
+Rscript -e "install.packages(c('assertthat', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'markdown', 'purrr', 'stringr'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())"
