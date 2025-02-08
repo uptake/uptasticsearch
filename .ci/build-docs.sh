3f07aee234ce92c0db8032f4ff8ca3bf22d2cffa
James Lamb <jaylamb20@gmail.com>
2025-02-07 22:26:40 -0600

    switch docs builds to macOS

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -3,22 +3,16 @@
 # failure is a natural part of life
 set -e -u -o pipefail
 
-# `devscripts` is required for 'checkbashisms' (https://github.com/r-lib/actions/issues/111)
-sudo apt-get update
-sudo apt-get install \
-    --no-install-recommends \
-    -y \
-    --allow-downgrades \
-    libcurl4-openssl-dev \
-    curl \
-    texinfo \
-    texlive-latex-recommended \
-    texlive-fonts-recommended \
-    texlive-fonts-extra \
-    tidy
+# setup LaTeX stuff
+brew install basictex
+export PATH="/Library/TeX/texbin:$PATH"
+sudo tlmgr --verify-repo=none update --self
+sudo tlmgr --verify-repo=none install inconsolata helvetic rsfs
 
+# install dependencies
 Rscript -e "install.packages(c('assertthat', 'data.table', 'futile.logger', 'httr', 'jsonlite', 'knitr', 'markdown', 'pkgdown', 'purrr', 'roxygen2', 'stringr', 'uuid'), repos = 'https://cran.r-project.org', Ncpus = parallel::detectCores())"
 
+# build the docs
 pushd ./r-pkg
 R CMD INSTALL --with-keep.source .
 Rscript -e "roxygen2::roxygenize()"
