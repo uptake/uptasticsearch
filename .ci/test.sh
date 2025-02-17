cb108a1a4c71f94cd2d8ea1919da82d73aa7c73c
James Lamb <jaylamb20@gmail.com>
2025-02-17 00:52:48 -0600

    get more logs

--- .ci/test.sh
+++ .ci/test.sh
@@ -10,7 +10,17 @@ R CMD INSTALL ./*.tar.gz
 pushd ./r-pkg/tests
 Rscript testthat.R
 
-exit 1
+echo "--- trying GET /_cat/indices ---"
+curl \
+    -i \
+    -X GET \
+    -H 'Accept: application/json' \
+    '127.0.0.1:9200/_cat/indices?format=json'
+
+echo "--- checking that the container is still up ---"
+docker ps
+
+exit 123
 
 export _R_CHECK_CRAN_INCOMING_=false
 R CMD check \
