6fe9b35eeaf95be46f3bf9b27e3482c4291944bc
James Lamb <jaylamb20@gmail.com>
2025-02-16 22:50:32 -0600

    one more version fix

--- setup_local.sh
+++ setup_local.sh
@@ -53,7 +53,7 @@ case "${ES_VERSION}" in
     docker run -d -p 9200:9200 \
         -e "discovery.type=single-node" \
         -e "xpack.security.enabled=false" \
-        docker.elastic.co/elasticsearch/elasticsearch:7.17.2
+        docker.elastic.co/elasticsearch/elasticsearch:7.17.22
     MAPPING_FILE=$(pwd)/test-data/es7_shakespeare_mapping.json
     SAMPLE_DATA_FILE=$(pwd)/test-data/sample_es7.json
     ;;
