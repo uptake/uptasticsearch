2bc43e0f409fe30d9c86e20b6405c1dfa415dfd4
James Lamb <jaylamb20@gmail.com>
2025-02-22 23:39:44 -0600

    get more logs

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,6 +4,11 @@
 set -e -u -o pipefail
 
 R CMD build ./r-pkg
+
+echo "--- running tests"
+cd r-pkg/tests/
+Rscript testthat.R
+
 export _R_CHECK_CRAN_INCOMING_=false
 R CMD check \
     --as-cran \
